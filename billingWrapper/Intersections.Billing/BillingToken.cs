3ac6cfd762fbbfdc6175c4cdc49c4ffda4257c28
ram.vallamsetty <rvallamsetty@gmail.com>
2019-01-22 11:42:51 -0500

    Code refactor
    Added a factory class to create an instance

--- billingWrapper/Intersections.Billing/BillingToken.cs
+++ billingWrapper/Intersections.Billing/BillingToken.cs
@@ -1,14 +1,8 @@
 ﻿using Intersections.Billing.Gateway;
-using Intersections.Billing.Gateway.Recurly;
 using Intersections.Billing.Model;
-using Intersections.Billing.Model.Recurly;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Configuration;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Intersections.Billing
@@ -36,11 +30,11 @@ namespace Intersections.Billing
 
                 if (!isValid)
                 {
-                    TokenResponse tokenResponse = new TokenResponse();                    
+                    TokenResponse tokenResponse = new TokenResponse();
                     foreach (var validationResult in results)
                     {
                         tokenResponse.addError(new Error(string.Join(", ", validationResult.MemberNames), validationResult.ErrorMessage));
-                    }                    
+                    }
                     return await Task.FromResult<TokenResponse>(tokenResponse);
                 }
                 else
@@ -50,22 +44,23 @@ namespace Intersections.Billing
                     return await billingGateway.getBillingToken(tokenRequest, isPreProd);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 TokenResponse tokenResponse = new TokenResponse();
                 tokenResponse.addError(new Error("unknown", "Unknown Exception"));
                 return tokenResponse;
             }
-        }  
-     
+        }
+
         /// <summary>
         /// Get Billing Gateway based on Billing Provider
         /// </summary>
         /// <returns></returns>
         private BillingGateway getBillingGateway()
         {
+            GatewayFactory factory = new GatewayFactory();
             if (billingProvider == "recurly")
-                return new RecurlyGateway();
+                return factory.getBillingGateway(billingProvider);
         }
     }
 }
---
+++ billingWrapper/Intersections.Billing/Gateway/GatewayFactory.cs
@@ -0,0 +1,21 @@
+﻿using Intersections.Billing.Gateway.Recurly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intersections.Billing.Gateway
+{
+    class GatewayFactory
+    {
+        public BillingGateway getBillingGateway(string gateway)
+        {
+            if (gateway == "recurly")
+            {
+                return new RecurlyGateway();
+            }
+            return null;
+        }
+    }
+}
